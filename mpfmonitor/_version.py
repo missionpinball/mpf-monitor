35adbcb9508c8c9a68442a032a6c657f4b64a24b
Jan Kantert <jan-mpf@kantert.net>
2021-07-25 23:46:15 +0200

    new dev version 0.56

--- mpfmonitor/_version.py
+++ mpfmonitor/_version.py
@@ -1,9 +1,9 @@
 # mpf-monitor
-__version__ = '0.55.0-dev.1'
-__short_version__ = '0.55'
+__version__ = '0.56.0-dev.1'
+__short_version__ = '0.56'
 __bcp_version__ = '1.1'
 __config_version__ = '5'
-__mpf_version_required__ = '0.55.0-dev.0'
+__mpf_version_required__ = '0.56.0-dev.1'
 
 version = "MPF Monitor v{} (config_version={}, BCP v{}, Requires MPF v{})".format(
     __version__, __config_version__, __bcp_version__, __mpf_version_required__)
