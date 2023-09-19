2c728ab73a4f2b26095013380e249d7d6caa5cac
Brian Madden <brian@fastpinball.com>
2023-09-19 08:46:50 -0700

    Add Python 3.10, 3.10, version 0.57.0.dev1

--- mpfmonitor/_version.py
+++ mpfmonitor/_version.py
@@ -1,7 +1,7 @@
 # mpf-monitor
-__version__ = '0.56.1'
-__short_version__ = '0.56'
+__version__ = '0.57.0.dev1'
+__short_version__ = '0.57'
 __bcp_version__ = '1.1'
-__config_version__ = '5'
+__config_version__ = '6'
 
 version = f"MPF Monitor v{__version__} (config_version={__config_version__}, BCP v{__bcp_version__})"
