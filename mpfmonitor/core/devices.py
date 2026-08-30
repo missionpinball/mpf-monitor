1642c6202a515b156c7ec9e614bc9e5c394a17e6
Bosh <alex@lobasc.io>
2026-08-30 03:04:24 -0700

    render device names for unplaced devices differently, to make scanning for missing devices easier

--- mpfmonitor/core/devices.py
+++ mpfmonitor/core/devices.py
@@ -167,6 +167,21 @@ class DeviceNode:
             self._callback = callback
             self.q_state.emitDataChanged()
 
+    def set_placed_status(self, is_placed: bool):
+        """Styles the node depending on whether it's placed on the playfield."""
+        self.q_name.setData(str(self._name), Qt.ItemDataRole.DisplayRole)
+        self.q_name.setData(None, Qt.ItemDataRole.FontRole)
+
+        if not is_placed:
+            bg_color = QApplication.palette().color(QPalette.ColorRole.Window)
+            is_dark_mode = bg_color.value() < 128
+
+            if is_dark_mode:
+                self.q_name.setData(QColor(100, 180, 255), Qt.ItemDataRole.ForegroundRole)
+            else:
+                self.q_name.setData(QColor(70, 100, 200), Qt.ItemDataRole.ForegroundRole)
+        else:
+            self.q_name.setData(None, Qt.ItemDataRole.ForegroundRole)
 
 class DeviceDelegate(QStyledItemDelegate):
     def __init__(self):
@@ -174,6 +189,10 @@ class DeviceDelegate(QStyledItemDelegate):
         super().__init__()
 
     def paint(self, painter, view, index):
+        if index.column() == 0:
+            super().paint(painter, view, index)
+            return
+
         super().paint(painter, view, index)
         color = None
         state = None
@@ -310,8 +329,8 @@ class DeviceDelegate(QStyledItemDelegate):
 
 class DeviceWindow(QWidget):
 
-    __slots__ = ["mpfmn", "ui", "model", "log", "already_hidden", "added_index", "device_states",
-                 "device_type_widgets", "_debug_enabled"]
+    __slots__ = ["mpfmon", "ui", "model", "log", "already_hidden", "added_index", "device_states",
+                 "device_type_widgets", "_debug_enabled", "filtered_model"]
 
     def __init__(self, mpfmon):
         self.mpfmon = mpfmon
@@ -402,6 +421,14 @@ class DeviceWindow(QWidget):
             node.setName(name)
             node.setData(state)
             node.setType(type)
+
+            is_placed = False
+            try:
+                if type in self.mpfmon.config and name in self.mpfmon.config[type]:
+                    is_placed = 'x' in self.mpfmon.config[type][name]
+            except (AttributeError, KeyError):
+                pass
+            node.set_placed_status(is_placed)
 
             self.device_states[type][name] = node
             self.device_type_widgets[type].appendRow(node.get_row())
